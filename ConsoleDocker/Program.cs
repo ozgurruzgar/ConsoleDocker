9d3aefcbf89b724d504cb02cd5acba202b762087
Özgür Rüzgar <94163712+ozgurruzgar@users.noreply.github.com>
2023-06-25 14:50:07 +0300

    Add project files.

---
+++ ConsoleDocker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleDocker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i < 1000)
+            {
+                Console.WriteLine("Naber " + i);
+                i++;
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
